6a59bfa16bc7f6eab7f461b6b10a8386c2c84a99
Hannes Struss <x@hannesstruss.de>
2015-02-08 18:28:55 +0100

    don't log generating the report

--- src/main/groovy/de/hannesstruss/godot/GodotPlugin.groovy
+++ src/main/groovy/de/hannesstruss/godot/GodotPlugin.groovy
@@ -1,11 +1,11 @@
 package de.hannesstruss.godot
 
-import com.google.gson.GsonBuilder
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.joda.time.DateTime
 
 class GodotPlugin implements Plugin<Project> {
+  private static final GENERATE_TASK_NAME = "generateGodotReport"
   private final gson = GsonFactory.get()
 
   @Override
@@ -13,11 +13,14 @@ class GodotPlugin implements Plugin<Project> {
     def start = System.currentTimeMillis()
 
     project.gradle.buildFinished { buildResult ->
-      def seconds = ((System.currentTimeMillis() - start) / 1000.0)
-      log(project, seconds.toDouble(), buildResult.getFailure() == null)
+      // don't log generating our report
+      if (project.gradle.startParameter.taskNames.findAll({ it.contains(GENERATE_TASK_NAME) }).isEmpty()) {
+        def seconds = ((System.currentTimeMillis() - start) / 1000.0)
+        log(project, seconds.toDouble(), buildResult.getFailure() == null)
+      }
     }
 
-    def generate = project.tasks.create("generateGodotReport", GenerateReportTask)
+    def generate = project.tasks.create(GENERATE_TASK_NAME, GenerateReportTask)
     generate.inputFile = new File("${project.rootDir}/godot.log")
     generate.outputDir = new File("${project.buildDir}/outputs/godot")
   }
