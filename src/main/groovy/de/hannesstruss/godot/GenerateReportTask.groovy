83037af7fb96fce13d3e4cf90926baa621048945
Hannes Struss <x@hannesstruss.de>
2015-02-20 15:31:58 +0100

    slightly more beautiful report with dimple/d3

--- src/main/groovy/de/hannesstruss/godot/GenerateReportTask.groovy
+++ src/main/groovy/de/hannesstruss/godot/GenerateReportTask.groovy
@@ -1,11 +1,14 @@
 package de.hannesstruss.godot
 
 import groovy.text.SimpleTemplateEngine
+import org.apache.commons.io.IOUtils
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 import org.joda.time.DateTime
 
 class GenerateReportTask extends DefaultTask {
+  private static final String[] STATIC_FILES = ["d3.js", "dimple.js"]
+
   File inputFile
 
   File outputDir
@@ -13,6 +16,12 @@ class GenerateReportTask extends DefaultTask {
   @TaskAction
   public void generate() {
     outputDir.mkdirs();
+
+    copyStaticFiles();
+    generateReportHtml();
+  }
+
+  private void generateReportHtml() {
     def outputFile = new File(outputDir, "report.html")
 
     def resource = getClass().getResource("/report/report.html")
@@ -21,7 +30,18 @@ class GenerateReportTask extends DefaultTask {
     engine.createTemplate(template).make(getData()).writeTo(new FileWriter(outputFile))
 
     println "Wrote Godot report to: $outputFile"
+  }
 
+  private void copyStaticFiles() {
+    File staticOutputDir = new File(outputDir, "assets")
+    staticOutputDir.mkdir()
+    STATIC_FILES.each { fileName ->
+      def is = getClass().getResource("/report/assets/${fileName}").openStream()
+      def os = new FileOutputStream(new File(staticOutputDir, fileName))
+      IOUtils.copy(is, os)
+      IOUtils.closeQuietly(is)
+      IOUtils.closeQuietly(os)
+    }
   }
 
   private Map getData() {
@@ -38,13 +58,13 @@ class GenerateReportTask extends DefaultTask {
     def oneMonthAgo = base.minusDays(30)
     def oneYearAgo = base.withYear(base.getYear() - 1)
 
-    def infos = [
-        PeriodInfo.fromBuilds("Today", models.findAll { it.loggedAt.isAfter(today) }),
-        PeriodInfo.fromBuilds("Last 30 days", models.findAll { it.loggedAt.isAfter(oneMonthAgo) }),
-        PeriodInfo.fromBuilds("Last Year", models.findAll { it.loggedAt.isAfter(oneYearAgo) })
-    ]
+//    def infos = [
+//        PeriodInfo.fromBuilds("Today", models.findAll { it.loggedAt.isAfter(today) }),
+//        PeriodInfo.fromBuilds("Last 30 days", models.findAll { it.loggedAt.isAfter(oneMonthAgo) }),
+//        PeriodInfo.fromBuilds("Last Year", models.findAll { it.loggedAt.isAfter(oneYearAgo) })
+//    ]
 
-    return [records: all, infos: infos ]
+    return [records: records]
   }
 
 
